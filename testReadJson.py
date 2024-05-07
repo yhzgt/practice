ae743765fd2e537103574b2bb5b7f8b983ec28ad
yhzgt <hangzhouyoung@gmail.com>
2024-05-07 13:56:32 +0800

    init
    test

---
+++ testReadJson.py
@@ -0,0 +1,8 @@
+import json
+
+
+with open('/Users/XYa116/Documents/python/oneBox/s3Path.txt', 'r') as f:
+    # Read the file line by line
+    for line in f:
+        # Do something with each line
+        print(line)
